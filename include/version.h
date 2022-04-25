2e010c449c8e970c7934894068970e90ff7e2142
ermiry <erick.salas@ermiry.com>
2022-04-24 20:25:13 -0500

    Version 0.1

--- include/version.h
+++ include/version.h
@@ -3,8 +3,8 @@
 
 #define PERCEPTHOR_VERSION				"0.1"
 #define PERCEPTHOR_VERSION_NAME			"Version 0.1"
-#define PERCEPTHOR_VERSION_DATE			"16/04/2022"
-#define PERCEPTHOR_VERSION_TIME			"14:44 CST"
+#define PERCEPTHOR_VERSION_DATE			"24/04/2022"
+#define PERCEPTHOR_VERSION_TIME			"20:25 CST"
 #define PERCEPTHOR_VERSION_AUTHOR		"Erick Salas"
 
 // print full percepthor version information
